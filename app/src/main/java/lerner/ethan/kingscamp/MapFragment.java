fe7dfe7626f459174f72c9687e1eb8b733dcf42e
yaffoman <yaffoman@gmail.com>
2019-09-06 13:35:51 -0700

    2 hrs, improved map image quality and implemented zoom

--- app/src/main/java/lerner/ethan/kingscamp/MapFragment.java
+++ app/src/main/java/lerner/ethan/kingscamp/MapFragment.java
@@ -1,17 +1,103 @@
 package lerner.ethan.kingscamp;
 
+import android.annotation.SuppressLint;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.FloatMath;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MapFragment extends Fragment {
+    ImageView imageDetail;
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    PointF startPoint = new PointF();
+    PointF midPoint = new PointF();
+    float oldDist = 1f;
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.map_layout, null);
+        View view = inflater.inflate(R.layout.map_layout, null);
+
+
+        imageDetail = (ImageView) view.findViewById(R.id.imageView2);
+        imageDetail.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                ImageView view = (ImageView) v;
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        savedMatrix.set(matrix);
+                        startPoint.set(event.getX(), event.getY());
+                        mode = DRAG;
+                        break;
+
+                    case MotionEvent.ACTION_POINTER_DOWN:
+
+                        oldDist = spacing(event);
+
+                        if (oldDist > 10f) {
+                            savedMatrix.set(matrix);
+                            midPoint(midPoint, event);
+                            mode = ZOOM;
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+
+                    case MotionEvent.ACTION_POINTER_UP:
+                        mode = NONE;
+
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        if (mode == DRAG) {
+                            matrix.set(savedMatrix);
+                            matrix.postTranslate(event.getX() - startPoint.x, event.getY() - startPoint.y);
+                        } else if (mode == ZOOM) {
+                            float newDist = spacing(event);
+
+                            if (newDist > 10f) {
+                                matrix.set(savedMatrix);
+                                float scale = newDist / oldDist;
+                                matrix.postScale(scale, scale, midPoint.x, midPoint.y);
+                            }
+                        }
+                        break;
+                }
+                view.setImageMatrix(matrix);
+
+                return true;
+            }
+
+            private float spacing(MotionEvent event) {
+                float x = event.getX(0) - event.getX(1);
+                float y = event.getY(0) - event.getY(1);
+                return (float) Math.sqrt(x * x + y * y);
+            }
+
+            private void midPoint(PointF point, MotionEvent event) {
+                float x = event.getX(0) + event.getX(1);
+                float y = event.getY(0) + event.getY(1);
+                point.set(x / 2, y / 2);
+            }
+        });
+        return view;
     }
 }
